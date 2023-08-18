952e972cb25844f0844e893aeb1035ba2704ae89
Voste126 <steveaustine126@gmail.com>
2023-08-18 22:54:26 +0300

    my commits

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+    
+    if length >= 1:
+        fib_sequence.append(0)
+    if length >= 2:
+        fib_sequence.append(1)
+    
+    while len(fib_sequence) < length:
+        next_num = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_num)
+    
+    print(fib_sequence)
+
